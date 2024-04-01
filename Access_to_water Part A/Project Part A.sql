6c323f33a170e2d54189856cd41d33fd5e85a975
charles nganga wairimu <charlesnganga06@gmail.com>
2024-04-01 11:57:49 +0300

    Update Project Part A.sql
    revised

--- Access_to_water Part A/Project Part A.sql
+++ Access_to_water Part A/Project Part A.sql
@@ -1,5 +1,5 @@
 -- PART 1 Beginning Our Data-Driven Journey in Maji Ndogo
--- 1. Get to know our data
+-- 1. Getting  to know our data
 
 SHOW TABLES	;
 /*data_dictionary
@@ -11,26 +11,25 @@ water_quality
 water_source
 well_pollution*/
 
-/*Let's use location so we can use that killer query, SELECT * but remember to limit it and tell
-it which table we are looking at.*/
+/*we us the location table to check whether the data was imported correctly */
 
 SELECT*
 FROM 
 	location ;
     
--- let's look at the visits table
+-- and also the visit table
 
 SELECT*
 FROM
 	visits;
     
--- let's look at the water_source table to see what a 'source' is. Normally "_id" columns are related to another table.
+--  water_source table to see what a 'source' is. Normally "_id" columns are related to another table.
     
 SELECT*
 FROM
 	water_source ;
 
---  write a SQL query to find all the unique types of water sources
+--   a SQL query to find all the unique types of water sources
 
 SELECT  DISTINCT
 	type_of_water_source
@@ -38,8 +37,7 @@ FROM
 	water_source;
     
 -- Unpack the visits to water sources
-/*Write an SQL query that retrieves all records from this table where the time_in_queue is more than some crazy time, say 500 min. How
-would it feel to queue 8 hours for water?*/
+/* SQL query that retrieves all records from this table where the time_in_queue is more than  500 min. */
 
 SELECT *
 FROM 
@@ -48,7 +46,7 @@ WHERE
 	time_in_queue >500
     ;
     
--- So let's writedown a couple of these source_id values from our results, and search for them in the other table.
+-- a couple of these source_id values from our results.
 
 SELECT *
 FROM
@@ -59,8 +57,8 @@ WHERE
     
 -- Assess the quality of water sources
 
-/* write a query to find records where the subject_quality_score is 10 -- only looking for home taps -- and where the source
-was visited a second time. What will this tell us?*/
+/* a query to find records where the subject_quality_score is 10 -- only looking for home taps -- and where the source
+was visited a second time.?*/
 
 SELECT
 	water_quality.record_id,
@@ -90,14 +88,14 @@ AND
 ;
     
 -- Investigate pollution issues
--- Find the right table and print the first few rows.
+-- Find the right table and print the first few rows to check a few rows.
 SELECT *
 FROM 
 	well_pollution 
 LIMIT 5
 ;
 
--- write a query that checks if the results is Clean but the biological column is > 0.01.
+--  a query that checks if the results is Clean but the biological column is > 0.01.
 
 SELECT*
 FROM 
@@ -107,7 +105,7 @@ WHERE
 AND 
 	results = 'clean'   
     ;
-    /* To find these descriptions, search for the word Clean with additional characters after it.
+    /* To find these descriptions, we will search for the word Clean with additional characters after it.
     As this is what separates incorrect descriptions from the records that should have "Clean".*/
     
 SELECT *
@@ -116,7 +114,7 @@ well_pollution
 WHERE description LIKE  ('clean_%')
 ;
 
-/*Looking at the results we can see two different descriptions that we need to fix:
+/*Looking at the results two different descriptions that need to fixed:
 1. All records that mistakenly have Clean Bacteria: E. coli should updated to Bacteria: E. coli*/
 UPDATE
   well_pollution 
@@ -206,12 +204,10 @@ biological > 0.01 AND results = 'Clean';
 DROP TABLE
 md_water_services.well_pollution_copy;
 
--- Additional Questions
--- Question 1
 
-/*You have been given a task to correct the phone number for the employee named 'Bello Azibo'. 
-The correct number is +99643864786. Write the SQL query to accomplish this.
-Note: Running these queries on the employee table may create issues later, 
+/*A query to  correct the phone number for the employee named 'Bello Azibo'. 
+The correct number is +99643864786.
+note :Running these queries on the employee table may create issues later, 
 so use the knowledge you have learned to avoid that.*/
 
 SELECT*
@@ -223,25 +219,5 @@ WHERE
         
 SET SQL_SAFE_UPDATES = 0;
 
-UPDATE
-    employee
-    SET phone_number= '+99643864786'
-WHERE
-	employee_name = 'Bello Azibo';
--- Question2
--- How many rows of data are returned for the following query?
--- (570 rows)
-
-SELECT COUNT(*)
-FROM well_pollution
-WHERE description
-IN ('Parasite: Cryptosporidium', 'biologically contaminated')
-OR (results = 'Clean' AND biological > 0.01);
-
--- Question 3
-
-SELECT COUNT(*)
-FROM well_pollution
-WHERE description LIKE 'Clean_%' OR results = 'Clean' AND biological < 0.01;
 
 
